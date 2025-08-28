646fa507370d8a25a7224b9acebfbe881b0c7477
itskheila <munenetiara@gmail.com>
2025-08-28 13:33:59 +0300

    dependancy installations

--- database/db.py
+++ database/db.py
@@ -1,71 +1,32 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base,sessionmaker
+from dotenv import load_dotenv
 import os
 
-Base = declarative_base()
 
-class Player(Base):
-    __tablename__ = 'players'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    games_played = Column(Integer, default=0)
-    games_won = Column(Integer, default=0)
 
-class GameHistory(Base):
-    __tablename__ = 'game_history'
-    
-    id = Column(Integer, primary_key=True)
-    player_name = Column(String, nullable=False)
-    dice_rolls = Column(String)
-    final_position = Column(Integer)
-    won = Column(Boolean)
-    game_date = Column(DateTime, default=datetime.now)
+load_dotenv()
 
-class GameDatabase:
-    def __init__(self, supabase_url=None, supabase_key=None):
-        if supabase_url and supabase_key:
-        
-            self.engine = create_engine(f'postgresql://{supabase_url}')
-        else:
-            
-            db_path = os.path.join(os.path.dirname(__file__), 'game.db')
-            self.engine = create_engine(f'sqlite:///{db_path}')
-        
-        Base.metadata.create_all(self.engine)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
-    
-    def add_player(self, name):
-        existing = self.session.query(Player).filter_by(name=name).first()
-        if not existing:
-            player = Player(name=name)
-            self.session.add(player)
-            self.session.commit()
-    
-    def save_game_result(self, player_name, dice_rolls, final_position, won):
-        dice_rolls_str = ','.join(map(str, dice_rolls))
-        
-        game = GameHistory(
-            player_name=player_name,
-            dice_rolls=dice_rolls_str,
-            final_position=final_position,
-            won=won
-        )
-        self.session.add(game)
-        
-        player = self.session.query(Player).filter_by(name=player_name).first()
-        if player:
-            player.games_played += 1
-            if won:
-                player.games_won += 1
-        
-        self.session.commit()
-    
-    def get_player_stats(self, name):
-        player = self.session.query(Player).filter_by(name=name).first()
-        if player:
-            return {'games_played': player.games_played, 'games_won': player.games_won}
-        return {'games_played': 0, 'games_won': 0}
+drivername=os.getenv("drivername")
+
+db_credentials={
+    "drivername":os.getenv("drivername"),
+    "host":os.getenv("host"),
+    "username":os.getenv("username"),
+    "password":os.getenv("password"),
+    "port":os.getenv("port"),
+    "database":os.getenv("database")
+}
+
+
+
+DATABASE_URL=URL.create(**db_credentials)
+
+engine=create_engine(DATABASE_URL,echo=False,future=True)
+
+
+SessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
+
+#BASE
+Base=declarative_base()
