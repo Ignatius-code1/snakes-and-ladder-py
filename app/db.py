d4bd7cda6b3e3c6989ebd81ae05c45a45e5a3457
itskheila <munenetiara@gmail.com>
2025-08-29 12:33:32 +0300

    correction

---
+++ app/db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base,sessionmaker
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine=create_engine(DATABASE_URL,echo=False,future=True)
+
+Base=declarative_base()
